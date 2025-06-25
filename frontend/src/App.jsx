fbdf1b76766e1ec88bc3d1bf8f3ca2212dd08aac
Shubham Kumawat <kumawatshubham2005@gmail.com>
2025-06-25 22:25:49 +0530

    BestSeller componet completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from "./components/Navbar.jsx"
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home.jsx'
-
+import { Toaster } from 'react-hot-toast'
 
 function App() {
 
@@ -11,6 +11,9 @@ function App() {
   return (
     <div>
     {isSellerPath ? null :  <Navbar/>}
+
+    <Toaster/>    
+    
       <div className={`${isSellerPath ? "": "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
    
    <Routes>
