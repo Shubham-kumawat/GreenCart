7f2bf3c6ce9877efc717cd558ec0bc5964136a28
Shubham Kumawat <kumawatshubham2005@gmail.com>
2025-07-01 12:44:13 +0530

    adding the middleware in project

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const allowedOrigins = ['http://localhost:5173'];
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cros({origin : allowedOrigins, credentials: true}));
+
 
 app.get('/', (req, res) => 
   res.send("APi is working!"));
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)});
+
