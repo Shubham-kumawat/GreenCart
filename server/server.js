76e495955d76bc5b9960d8a0039dd75fde1dd77d
Shubham Kumawat <kumawatshubham2005@gmail.com>
2025-07-01 14:16:19 +0530

    database connected

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import cors from 'cors';
+import connectDB from './configs/db.js';
+import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+await connectDB()
 
 const allowedOrigins = ['http://localhost:5173'];
 
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cros({origin : allowedOrigins, credentials: true}));
+app.use(cors({origin : allowedOrigins, credentials: true}));
 
 
 app.get('/', (req, res) => 
