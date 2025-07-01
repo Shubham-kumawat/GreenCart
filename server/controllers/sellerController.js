b9f17bd05118d15cbd82a674e6a16c210ec6813d
Shubham Kumawat <kumawatshubham2005@gmail.com>
2025-07-01 16:52:06 +0530

    api for seller login logout completed

---
+++ server/controllers/sellerController.js
@@ -0,0 +1,60 @@
+//login seller : /api/seller/login
+
+export const sellerLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (
+      password === process.env.SELLER_PASSWORD &&
+      email === process.env.SELLER_EMAIL
+    ) {
+      const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+
+      res.cookie("sellerToken", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week cookie expiration time
+      });
+
+      return res.json({ success: true, message: "Login successful" });
+    } else {
+      return res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//seller auth : /api/seller/is-auth
+
+
+export const isSellerAuth = async (req, res) => {
+    try{
+        
+        return res.json({ success: true });
+
+    }catch (error) { 
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+
+//sellerlogout : /api/seller/logout
+
+export const sellerLogout = async (req, res) => {
+    try{
+        res.clearCookie("sellerToken", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production'? 'none' : 'strict',
+        });
+        return res.json({ success: true, message: 'Logged out' });
+    }catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import connectDB from './configs/db.js';
 import 'dotenv/config';
 import userRouter from './routes/userRoute.js';
+import sellerRouter from './routes/sellerRoute.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -22,6 +23,7 @@ app.get('/', (req, res) =>
   res.send("APi is working!"));
 
 app.use('/api/user',userRouter)
+app.use('/api/seller',sellerRouter)
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)});
