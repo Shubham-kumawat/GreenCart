631a10e20d024928c5b1d7a2d3d42e95710550a7
Shubham Kumawat <kumawatshubham2005@gmail.com>
2025-07-02 11:25:24 +0530

    product controller functions createion is complete

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -31,17 +31,40 @@ try {
 //get product : /api/product/list
 
 export const productList = async (req, res) => {
+        try {
+                const products = await Product.find({})
+                res.json({ success: true, products})
+                
+        } catch (error) {
+               console.log(error.message);
+        res.json({ success: false, message: error.message})   
+        }
 
 }
 
 // get single product by id : /api/product/id
 
 export const productById = async (req, res) => {
+        try {
+                const {id} = req.body
+                const product = await Product.findById(id)
+                res.json({ success: true, product})
+        } catch (error) {
+                console.log(error.message);
+        res.json({ success: false, message: error.message})
+        }
 
 }
 
 // change product inStock : /api/product/stock
 
 export const changeStock = async (req, res) => {
-
+        try {
+                const {id, inStock} = req.body
+                await Product.findByIdAndUpdate(id, {inStock})
+                res.json({ success: true, message: "Product stock updated successfully"})
+        } catch (error) {
+         console.log(error.message);
+        res.json({ success: false, message: error.message})
+        }
 }
