8737afb290613a0f3d3c31f4430ad292d046590e
Shubham Kumawat <kumawatshubham2005@gmail.com>
2025-07-04 12:51:46 +0530

    seller logout features is done with backend

--- frontend/src/pages/seller/SellerLayout.jsx
+++ frontend/src/pages/seller/SellerLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useAppContext } from '../../context/AppContext';
 import { assets } from '../../assets/assets';
-import { Link, NavLink, Outlet } from 'react-router-dom';
+import { Link, Navigate, NavLink, Outlet } from 'react-router-dom';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const SellerLayout = () => {
 
-    const {setIsSeller}= useAppContext();
+    const {axios ,navigate}= useAppContext();
 
   
     const sidebarLinks = [
@@ -15,7 +17,17 @@ const SellerLayout = () => {
     ];
 
     const logout = async ()=>{
-        setIsSeller(false);
+        try {
+             const {data} = await axios.get('/api/seller/logout');
+            if(data.success){
+                toast.success(data.message);
+                navigate('/')
+            } else {
+                toast.error(data.message);
+            }
+            } catch (error) {
+            toast.error(error.message);
+        }
     }
 
     return (
