63c3777a591571dabcefa372bb8847eaf3094c9c
Shubham Kumawat <kumawatshubham2005@gmail.com>
2025-07-10 14:15:57 +0530

    logo changed in footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -10,7 +10,10 @@ const Footer = () =>{
         <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-24 bg-green-50 ">
             <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
                 <div>
-                    <img className="w-34 md:w-32" src={assets.logo} alt="dummyLogoColored" />
+                    <div className="flex items-center">
+          <img  className="h-9 " src="/src/assets/favicon.png" alt="logo" />
+        <p className="text-green-700 font-bold text-3xl">Grocer<span className="text-orange-500 text-3xl font-bold">in</span></p>
+        </div>
                     <p className="max-w-[410px] mt-6">We deliver fresh groceries and snacks straight to your door. Trusted by thousands, we aim to make your shopping experience simple and affordable.</p>
                 </div>
                 <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
