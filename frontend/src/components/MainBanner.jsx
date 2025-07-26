01a448ce79bd788a878a3d043e40d71eed3678ba
Shubham Kumawat <kumawatshubham2005@gmail.com>
2025-07-26 09:50:00 +0530

    updated

--- frontend/src/components/MainBanner.jsx
+++ frontend/src/components/MainBanner.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 function MainBanner() {
   return (
     <div className='relative '>
-        <img src={assets.main_banner_bg} alt="banner" className='w-full hidden md:block' />
+        <img src={assets.main_banner_bg} alt="banner" className='w-full  hidden md:block' />
         <img src={assets.main_banner_bg_sm} alt="banner" className='w-full md:hidden ' />
         
         <div className='absolute inset-0 flex flex-col items-center justify-end md:items-start  md:justify-center pb-24 md:pd-0 mt-10 md:pl-18 lg:pl-24 '>
